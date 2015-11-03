08192e80977deb46aada16df21536f1e178d381b
vrenc <vrencvd@gmail.com>
2015-11-03 22:46:45 +0100

    Add uploadHandler

--- app.go
+++ app.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"image"
 	"image/draw"
 	"image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 	"net/http"
+	"os"
 
 	"github.com/codegangsta/negroni"
 )
@@ -15,11 +18,31 @@ func main() {
 	n := negroni.Classic()
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/upload", imageHandler)
+	mux.HandleFunc("/upload", uploadHandler)
+	mux.HandleFunc("/img", imageHandler)
 
 	n.UseHandler(mux)
 
 	n.Run(":3000")
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	bgForm, _, _ := r.FormFile("background")
+	imgForm, _, _ := r.FormFile("image")
+
+	bg, _, _ := image.Decode(bgForm)
+	img, _, _ := image.Decode(imgForm)
+
+	hasher := sha256.New()
+	bgFileName := hasher.Sum([]byte("bg"))
+	bgFile, _ := os.Create(fmt.Sprintf("./storage/%x", bgFileName))
+	defer bgFile.Close()
+	gif.Encode(bgFile, bg, nil)
+
+	imgFileName := hasher.Sum([]byte("img"))
+	imgFile, _ := os.Create(fmt.Sprintf("./storage/%x", imgFileName))
+	defer imgFile.Close()
+	gif.Encode(imgFile, img, nil)
 }
 
 func imageHandler(w http.ResponseWriter, r *http.Request) {
