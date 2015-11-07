0c4d6061ba1b770673b51f0866f48aa77a7baf6a
vrenc <vrencvd@gmail.com>
2015-11-08 00:15:20 +0100

    Use JWT issuer when generating file name hashes

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"image"
 	"image/draw"
@@ -11,6 +12,8 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func getFileName(imgName string) string {
@@ -20,34 +23,68 @@ func getFileName(imgName string) string {
 	return fmt.Sprintf(dir+"%x", imgFileName)
 }
 
+func sendResponse(w http.ResponseWriter, status int, data interface{}) {
+	w.WriteHeader(status)
+	enc := json.NewEncoder(w)
+	enc.Encode(data)
+}
+
+// Upload handler
+// Stores the POSTed GIF and overlay
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	token, _ := jwt.ParseFromRequest(r, hmacKeyFunc)
+	issuer := token.Claims["iss"].(string)
+	fmt.Println(issuer)
 	gfForm, _, err := r.FormFile("gif")
 	overlayForm, _, err := r.FormFile("overlay")
 
 	if err != nil {
-		fmt.Fprint(w, "error reading images")
-		panic(err)
+		sendResponse(w, 400, map[string]string{
+			"error": "Error while reading images",
+		})
+		return
 	}
 
-	gf, _ := gif.DecodeAll(gfForm)
-	overlay, _, _ := image.Decode(overlayForm)
+	gf, err := gif.DecodeAll(gfForm)
+	overlay, _, err := image.Decode(overlayForm)
 
-	gfFile, _ := os.Create(getFileName("gif"))
+	if err != nil {
+		sendResponse(w, 400, map[string]string{
+			"error": "Error while decoding images",
+		})
+		return
+	}
+
+	gfFile, err := os.Create(getFileName("gif:" + issuer))
 	defer gfFile.Close()
 	gif.EncodeAll(gfFile, gf)
 
-	overlayFile, _ := os.Create(getFileName("overlay"))
+	overlayFile, err := os.Create(getFileName("overlay:" + issuer))
 	defer overlayFile.Close()
 	png.Encode(overlayFile, overlay)
-}
-
-func imageHandler(w http.ResponseWriter, r *http.Request) {
-	gfFile, err := os.Open(getFileName("gif"))
-	overlayFile, err := os.Open(getFileName("overlay"))
 
 	if err != nil {
-		fmt.Fprint(w, "error reading images")
-		panic(err)
+		sendResponse(w, 400, map[string]string{
+			"error": "Error while saving images",
+		})
+		return
+	}
+}
+
+// Image Handler
+// Reads the stored GIF & overlay for the authenticated user and returns a new GIF with the overlay copied onto each frame of the input GIF
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	token, _ := jwt.ParseFromRequest(r, hmacKeyFunc)
+	issuer := token.Claims["iss"].(string)
+	fmt.Println(issuer)
+	gfFile, err := os.Open(getFileName("gif:" + issuer))
+	overlayFile, err := os.Open(getFileName("overlay:" + issuer))
+
+	if err != nil {
+		sendResponse(w, 400, map[string]string{
+			"error": "Error while reading images",
+		})
+		return
 	}
 
 	// Decode images
@@ -55,8 +92,10 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 	src, _, err := image.Decode(overlayFile)
 
 	if err != nil {
-		fmt.Fprint(w, "error decoding images")
-		panic(err)
+		sendResponse(w, 400, map[string]string{
+			"error": "Error while decoding images",
+		})
+		return
 	}
 
 	// Create location for the draw
